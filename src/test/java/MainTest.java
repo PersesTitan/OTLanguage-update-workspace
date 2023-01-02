a6f7ba30c97a46277c50d920c867d663fa638a47
PersesTitan <torampersesTitan@gmail.com>
2023-01-03 00:28:48 +0900

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,21 @@
+import bin.apply.Setting;
+import bin.apply.item.AccessMap;
+
 public class MainTest {
     public static void main(String[] args) {
-        Class<?> klass = Integer.class;
-
-        System.out.println(klass.getName());
-        System.out.println(klass.getTypeName());
+        new CreateModule().create();
+        AccessMap map = new AccessMap();
+        String total = """
+                ㅆㅁㅆ 앙
+                ㅇㅅㅇ 변수:1.2
+                ㅆㅁㅆ :변수_
+                
+                ㅇㅂㅇ 변수1:ㅇㅇ
+                ㅇㅂㅇ 변수2:ㄴㄴ
+                ㄹㅂㄹ 변수3:[ㄴㄴ, ㅇㅇ]
+                
+                ㅆㅁㅆ :변수1_ :변수2_ :변수3_
+                """;
+        total.lines().forEach(v -> Setting.start(v, v, map));
     }
 }

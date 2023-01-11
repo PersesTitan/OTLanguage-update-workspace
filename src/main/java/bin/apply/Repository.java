f5c32dd840ba0ff2375fedffc1d9bc70b644ed9d
PersesTitan <torampersesTitan@gmail.com>
2023-01-12 00:15:27 +0900

    Update Repository.java

--- src/main/java/bin/apply/Repository.java
+++ src/main/java/bin/apply/Repository.java
@@ -1,5 +1,22 @@
 package bin.apply;
 
-public interface Repository {
+import bin.apply.item.map.TypeMap;
+import bin.exception.VariableException;
+import work.ReplaceWork;
+import work.StartWork;
 
+import java.util.*;
+
+public interface Repository {
+    LinkedList<TypeMap> repositoryArray = new LinkedList<>();
+    Set<String> types = new HashSet<>();
+
+    Map<String, Map<String, StartWork>> startWorks = new HashMap<>();
+    Map<String, Map<String, ReplaceWork>> replaceWorks = new HashMap<>();
+    Set<String> defineWordList = new HashSet<>();
+
+    static void addDefineWord(String word) {
+        if (defineWordList.contains(word)) throw new VariableException().defineType(word);
+        else defineWordList.add(word);
+    }
 }

2d696dc4de160a8e2383c73f10d26d1f334fdb52
PersesTitan <torampersesTitan@gmail.com>
2023-01-23 14:14:28 +0900

    Create DebugMode.java

---
+++ src/main/java/bin/apply/item/DebugMode.java
@@ -0,0 +1,31 @@
+package bin.apply.item;
+
+public enum DebugMode {
+    // Level = H >>> L
+    INFO, WARRING, ERROR, COMPILE;
+
+    private final int level;
+    DebugMode() {
+        this.level = this.ordinal();
+    }
+
+    DebugMode(int level) {
+        this.level = level;
+    }
+    /**
+     * ex) INFO.check(DebugMode.WARRING) <br>
+     *               => 1 < 2            <br>
+     *               => true             <br>
+     */
+     public boolean check(DebugMode debugMode) {
+        return this.level < debugMode.level;
+    }
+
+    public boolean isCompile() {
+         return this.equals(COMPILE);
+    }
+
+    public boolean isNoCompile() {
+         return !isCompile();
+    }
+}

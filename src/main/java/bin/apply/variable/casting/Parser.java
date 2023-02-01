1d87c5cc2ae55f0054791884cd979487337c3143
PersesTitan <torampersesTitan@gmail.com>
2023-02-01 15:05:56 +0900

    Create Parser.java

---
+++ src/main/java/bin/apply/variable/casting/Parser.java
@@ -0,0 +1,11 @@
+package bin.apply.variable.casting;
+
+import bin.apply.Repository;
+import bin.apply.repository.AccessList;
+
+public class Parser implements Repository {
+    public void parser(String line) {
+        // ㅇㅁㅇ 변수명:값
+        
+    }
+}

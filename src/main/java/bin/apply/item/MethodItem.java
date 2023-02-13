748d5a9efa3d972e112b2a8ecb5ffab983ce851d
PersesTitan <torampersesTitan@gmail.com>
2023-02-14 01:22:03 +0900

    Delete MethodItem.java

--- src/main/java/bin/apply/item/MethodItem.java
+++
@@ -1,8 +0,0 @@
-package bin.apply.item;
-
-import java.util.Map;
-
-public record MethodItem(String name, Map<Long, String> repository,
-                         long start, long end, boolean isLoop,
-                         ParamItem...params) {
-}

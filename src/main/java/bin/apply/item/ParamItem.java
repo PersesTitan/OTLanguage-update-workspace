30ea0b9d0e60d4a79081eebdc524d162d8675a29
PersesTitan <torampersesTitan@gmail.com>
2023-02-14 01:22:08 +0900

    Delete ParamItem.java

--- src/main/java/bin/apply/item/ParamItem.java
+++
@@ -1,9 +0,0 @@
-package bin.apply.item;
-
-import bin.apply.Repository;
-
-public record ParamItem(String type, String name) {
-    public Object casting(String value) {
-        return Repository.klassItems.get(type).casting().apply(value);
-    }
-}

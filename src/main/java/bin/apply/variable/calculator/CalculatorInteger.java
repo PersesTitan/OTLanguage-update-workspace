e6e4e4d3250d469ce2de9332ba6c146ef6ab9cf6
PersesTitan <torampersesTitan@gmail.com>
2023-02-19 00:59:17 +0900

    Create CalculatorInteger.java

---
+++ src/main/java/bin/apply/variable/calculator/CalculatorInteger.java
@@ -0,0 +1,17 @@
+package bin.apply.variable.calculator;
+
+import bin.apply.item.CalculatorItem;
+import bin.exception.MatchException;
+import bin.repository.AccessList;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.function.BiFunction;
+
+public final class CalculatorInteger implements Calculator {
+    public int calculator(AccessList repositoryArray, String line) {
+        return -1;
+    }
+}

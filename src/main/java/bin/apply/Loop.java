fe2352e8a9e5219cc080386f2712a7cc422d20b3
PersesTitan <torampersesTitan@gmail.com>
2023-02-21 01:02:41 +0900

    Update Loop.java

--- src/main/java/bin/apply/Loop.java
+++ src/main/java/bin/apply/Loop.java
@@ -1,51 +1,59 @@
 package bin.apply;
 
+import bin.apply.item.LoopItem;
 import bin.exception.MatchException;
 import bin.token.Token;
 
 import java.util.Map;
 import java.util.Stack;
 
-public class Loop implements Token {
-    public int next(Map<Integer, String> code, int position) {
+public class Loop {
+    public static int next(Map<Integer, String> code, int position) {
         Stack<Integer> stack = new Stack<>();
         int size = code.size();
         for (int i = position + 1; i<size; i++) {
             String line = code.get(i);
             if (line.isEmpty()) continue;
-            if (line.startsWith(LOOP_S)) {
+            if (line.startsWith(Token.LOOP_S)) {
                 if (stack.isEmpty()) return i;
                 else stack.pop();
-            } else if (line.endsWith(LOOP_E)) stack.add(i);
+            } else if (line.endsWith(Token.LOOP_E)) stack.add(i);
         }
         throw MatchException.ZONE_MATCH_ERROR.getThrow(code.get(position));
     }
 
     // ex) value : } => 변수명
-    public String getReturn(String value) {
-        int i = value.indexOf(RETURN_TOKEN) + RETURN_TOKEN.length();
+    public static String getReturn(String value) {
+        int i = value.indexOf(Token.RETURN_TOKEN) + Token.RETURN_TOKEN.length();
         return value.substring(i).strip();
     }
 
     // ex) value : } <= ㅇㅁㅇ 변수명
-    public String getPut(String value) {
-        int i = value.indexOf(PUT_TOKEN) + PUT_TOKEN.length();
+    public static String getPut(String value) {
+        int i = value.indexOf(Token.PUT_TOKEN) + Token.PUT_TOKEN.length();
         return value.substring(i).strip();
     }
 
-    public boolean isReturn(String value) {
-        int i = value.indexOf(RETURN_TOKEN);
-        return value.startsWith(LOOP_E)
+    public static boolean isReturn(String value) {
+        int i = value.indexOf(Token.RETURN_TOKEN);
+        return value.startsWith(Token.LOOP_E)
                 && i != -1
                 && value.substring(1, i).isBlank()
-                && !value.substring(i + RETURN_TOKEN.length()).isBlank();
+                && !value.substring(i + Token.RETURN_TOKEN.length()).isBlank();
     }
 
-    public boolean isPut(String value) {
-        int i = value.indexOf(PUT_TOKEN);
-        return value.startsWith(LOOP_E)
+    public static boolean isPut(String value) {
+        int i = value.indexOf(Token.PUT_TOKEN);
+        return value.startsWith(Token.LOOP_E)
                 && i != -1
                 && value.substring(1, i).isBlank()
-                && !value.substring(i + PUT_TOKEN.length()).isBlank();
+                && !value.substring(i + Token.PUT_TOKEN.length()).isBlank();
+    }
+
+    public static LoopItem getLoopType(String value) {
+        if (value.equals(Token.LOOP_S)) return LoopItem.NONE;   // }
+        else if (isPut(value)) return LoopItem.PUT;                               // } <= ㅇㅁㅇ 변수명
+        else if (isReturn(value)) return LoopItem.RETURN;                         // } => 변수명
+        else return LoopItem.NOT;
     }
 }

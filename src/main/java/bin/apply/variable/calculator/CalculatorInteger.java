81fd363e0acac436bf2d8f68fd4ce2284ee65f06
PersesTitan <torampersesTitan@gmail.com>
2023-02-23 00:09:37 +0900

    Update CalculatorInteger.java

--- src/main/java/bin/apply/variable/calculator/CalculatorInteger.java
+++ src/main/java/bin/apply/variable/calculator/CalculatorInteger.java
@@ -1,17 +1,26 @@
 package bin.apply.variable.calculator;
 
 import bin.apply.item.CalculatorItem;
+import bin.apply.variable.Parser;
+import bin.apply.variable.calculator.number.NumberItem;
 import bin.exception.MatchException;
 import bin.repository.AccessList;
+import bin.token.KlassToken;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.lang.constant.Constable;
+import java.lang.constant.ConstantDesc;
+import java.util.*;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 public final class CalculatorInteger implements Calculator {
-    public int calculator(AccessList repositoryArray, String line) {
+    public int calculator(String line) {
+        Stack<Object> stack = this.parser(line)
+                .stream()
+                .map(v -> {
+                    if (NumberItem.contains(v)) return v;
+                    else return Parser.parser(KlassToken.INT_VARIABLE, v.strip());
+                }).collect(Collectors.toCollection(Stack::new));
         return -1;
     }
 }

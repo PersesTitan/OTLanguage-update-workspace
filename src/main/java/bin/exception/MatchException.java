1afca8a18b2fa05e4a4bf70a27a25afafdd68c4a
PersesTitan <torampersesTitan@gmail.com>
2023-02-24 10:57:33 +0900

    Update MatchException.java

--- src/main/java/bin/exception/MatchException.java
+++ src/main/java/bin/exception/MatchException.java
@@ -1,17 +1,21 @@
 package bin.exception;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+@Getter
 @RequiredArgsConstructor
 public enum MatchException implements ExceptionTool {
     GRAMMAR_ERROR("문법이 일치하지 않습니다."),
     PARAM_COUNT_ERROR("메소드의 파리미터 길이가 유효하지 않습니다."),
-    ZONE_ERROR("존을 사용할 수 없거나 사용해야합니다."),
+    ZONE_MATCH_ERROR("괄호가 일치하지 않아 존을 사용할 수 없습니다."),
     DEFINE_CLASS_NAME("이미 정의된 클래스 이름입니다."),
     DEFINE_METHOD_NAME("이미 정의된 메소드 이름입니다."),
     CALCULATOR_ERROR("계산식 중 유효하지 않는 토큰이 존재합니다."),
+    PARAM_MATCH_ERROR("파라미터 문법이 일치하지 않습니다."),
+    MAP_MATCH_ERROR("맵에서는 =를 사용하여 값을 구분해주어야합니다.")
     ;
 
     private AtomicReference<String> errorCode;
@@ -30,9 +34,9 @@ public enum MatchException implements ExceptionTool {
                     The parameter length of the method is not valid. %s
                     Please check the method parameters.
                     """;
-            case ZONE_ERROR ->
+            case ZONE_MATCH_ERROR ->
                     """
-                    Zone is not available or must be used. %s
+                    Zone is not available because parentheses do not match. %s
                     Please check the grammar.
                     """;
             case DEFINE_CLASS_NAME ->
@@ -50,22 +54,22 @@ public enum MatchException implements ExceptionTool {
                     An invalid token exists in the calculation expression. %s
                     Please check the calculation formula.
                     """;
+            case PARAM_MATCH_ERROR ->
+                    """
+                    Parameter grammar does not match. %s
+                    Please check the grammar again.
+                    """;
+            case MAP_MATCH_ERROR ->
+                    """
+                    In maps, you must use = to separate values. %s
+                    Please check the grammar again.
+                    """;
         };
     }
 
     @Override
-    public String getErrorCode() {
-        return "(" + this.errorCode.getAndSet(null) + ")";
-    }
-
-    public MatchError getThrow(String errorCode) {
+    public Error getThrow(String errorCode) {
         if (this.errorCode == null) this.errorCode = new AtomicReference<>(errorCode);
-        return new MatchError(this.message);
-    }
-
-    public static class MatchError extends RuntimeException {
-        public MatchError(String message) {
-            super(message);
-        }
+        return new Error(this.name(), this.getClass());
     }
 }
